87dff26c46e1ef5aa175d51992e22ac94bc2ee87
alexkimeu0 <kim3ual3x@gmail.com>
2021-01-12 21:22:00 +0300

    Did the header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBukCdqKsg1Ds0JX9VykyvBCSrSTScSOJ0",
+    authDomain: "fir-2a8cc.firebaseapp.com",
+    projectId: "fir-2a8cc",
+    storageBucket: "fir-2a8cc.appspot.com",
+    messagingSenderId: "13862848273",
+    appId: "1:13862848273:web:c7f2682b198a78c1f0eeb0",
+    measurementId: "G-8038K4CCTJ"
+  };
